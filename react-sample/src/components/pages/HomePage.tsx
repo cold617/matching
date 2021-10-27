991745bb1f9fa172e8b9da823a64c1bd3dd7c33e
Hodaka Ito <2190498@ecccomp.com>
2021-10-27 12:29:57 +0900

    main

---
+++ react-sample/src/components/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Template from "../templates/template";
+
+const HomePage: React.FC = () => {
+    return (
+        <Template title="トップページ">
+            <>トップ内容</>
+        </Template>
+    );
+};
+
+export default HomePage;
