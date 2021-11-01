598277a080aa43e92840fc4113bb382d91bfafa6
Hodaka Ito <2190498@ecccomp.com>
2021-11-01 13:47:18 +0900

    あ

--- react-sample/src/components/pages/ProductPage.tsx
+++ react-sample/src/components/pages/ProductPage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Template from "../templates/template";
 
 const ProductPage: React.FC = () =>{
-    return <>商品ページ</>;
+    return (
+        <Template title="商品ページ">
+            <>商品内容</>
+        </Template>
+    );
 };
 
 export default ProductPage;
